ede4d5174747c9f58cadd278efe4fac32bbde176
jevansbio <32939196+jevansbio@users.noreply.github.com>
2023-03-21 22:36:26 +0100

    Classes, extra args, end of section 3

--- inflammation/models.py
+++ inflammation/models.py
@@ -15,6 +15,56 @@ Functions:
 
 import numpy as np
 
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+class Person:
+    """A person."""
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name, observations=None):
+        super().__init__(name)
+        self.observations = []
+        if observations is not None:
+            self.observations = observations
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(day, value)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+class Doctor(Person):
+    """A doctor in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, new_patient):
+        # A crude check by name if this patient is already looked after
+        # by this doctor before adding them
+        for patient in self.patients:
+            if patient.name == new_patient.name:
+                return
+        self.patients.append(new_patient)
 
 def load_csv(filename):
     """Load a Numpy array from a CSV
@@ -77,3 +127,31 @@ def patient_normalise(data):
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
+
+from functools import reduce
+
+...
+
+def daily_above_threshold(patient_num, data, threshold):
+   """Count how many days a given patient's inflammation exceeds a given threshold.
+
+   :param patient_num: The patient row number
+   :param data: A 2D data array with inflammation data
+   :param threshold: An inflammation threshold to check each daily value against
+   :returns: An integer representing the number of days a patient's inflammation is over a given threshold
+   """
+
+   above_threshold = [x>threshold for x in data[patien_num]]
+   return reduce(lambda a, b: a + 1 if b else a, above_threshold, 0)
+
+def attach_names(data, names):
+    """Create datastructure containing patient records."""
+    assert len(data) == len(names)
+    output = []
+
+    for data_row, name in zip(data, names):
+        output.append({'name': name,
+                       'data': data_row})
+
+    return output
+
