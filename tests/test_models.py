546491818859b36828d56ab4c2833fb10ab17063
jevansbio <32939196+jevansbio@users.noreply.github.com>
2023-03-21 20:31:00 +0100

    Extra preconditions in patient normalise

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,19 +8,20 @@ import pytest
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
     ])
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
     npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [5, 6]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [5, 6]),
         ([[-1, 2], [3, -4], [-5, 6]], [3, 6]),
     ])
 def test_daily_max(test, expected):
@@ -28,11 +29,12 @@ def test_daily_max(test, expected):
     from inflammation.models import daily_max
     npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
+
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
-        ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [1, 2]),
         ([[-1, 2], [3, -4], [-5, 6]], [-5, -4]),
     ])
 def test_daily_min(test, expected):
@@ -41,10 +43,46 @@ def test_daily_min(test, expected):
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], None),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], None),
+        (
+                [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                ValueError,
+        ),
+        (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+        ),
+        (
+                'hello',
+                None,
+                TypeError,
+        ),
+        (
+                3,
+                None,
+                TypeError,
+        ),
+        (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+        )
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    
+    if isinstance(test, list):
+        test = np.array(test)
 
-def test_daily_min_string():
-    """Test for TypeError when passing strings"""
-    from inflammation.models import daily_min
-
-    with pytest.raises(TypeError):
-        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
